5222e1cf7573ec8909b2eaeb8351bae613aff8b1
vbonnard <vbonnard.dev@gmail.com>
2026-04-24 22:36:55 +0200

    fix migration

---
+++ transcendence/backend/accounts/migrations/0009_merge_20260424_2235.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0007_localuser_games_draw_localuser_games_lost'),
+        ('accounts', '0008_alter_localuser_client_prefs'),
+    ]
+
+    operations = []
